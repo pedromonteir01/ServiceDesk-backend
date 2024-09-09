36e564b89fc0009c902a57fdc43e6f7493a8a7a9
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-09-09 09:14:31 -0300

    feat: add comment

--- src/database/database.config.js
+++ src/database/database.config.js
@@ -1,7 +1,7 @@
 //imports
 const { Pool } = require('pg'); 
 
-//config without dotenv and local
+//config without dotenv and local db for tests
 const pool = new Pool({
     host: 'localhost',
     port: 7777,
