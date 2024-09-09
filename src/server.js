b351cbadc28e11a9bee575c908650553c80abb51
caiquenaimi <caique.naimi@aluno.senai.br>
2024-09-09 09:03:24 -0300

    chore: Update npm dependencies and add CORS support

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
-const express = require('express'); // O express serve para criar o servidor
-const { config } = require('dotenv'); // O dotenv serve para ler as variáveis de ambiente
-
-config(); // Carrega as variáveis de ambiente
-
-const port = process.env.PORT || 5000; // Pega a porta do arquivo .env ou usa a porta 5000
+require("dotenv").config(); // Carrega as variáveis de ambiente
+const express = require("express"); // O express serve para criar o servidor
+const cors = require("cors"); // O cors serve para permitir requisições de outros domínios
 
 const app = express(); // Cria o servidor e armazena na variável app
+const port = process.env.PORT || 5000; // Pega a porta do arquivo .env ou usa a porta 5000
+
 app.use(express.json()); // Configura o servidor para receber requisições com o formato JSON
+
+app.use(cors()); // Configura o servidor para aceitar requisições de outros domínios
 
 // Inicia o servidor na porta configurada
 app.listen(port, () =>
