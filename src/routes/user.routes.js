9df6893b41668a607311ac6d6e3bb3b0b5741218
ArthBG <arthurborges.senai564@gmail.com>
2024-09-09 09:56:24 -0300

    fetch: update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,7 @@ const usersController = require('../controllers/userController');
 router.post('/user', usersController.createUser);
 router.get('/user', usersController.getUser);
 router.get('/user/:name', usersController.getUserByName);
+router.get('/user/:email', usersController.getUserByEmail);
 router.delete('/user/:email', usersController.deleteUser);
 
 module.exports = router;
