6e15ed3c577f5ad3c1a35c9530a3c1f5504a66d0
ArthBG <arthurborges.senai564@gmail.com>
2024-09-09 10:49:36 -0300

    fix: index.routes.js refactoring

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,1 +1,12 @@
-const routes = Router();
+const express = require('express');
+const router = express.Router();
+const requestRoute = require('./request.routes')
+const userRoute = require('./user.routes')
+
+router.get('/', (req, res) => {
+    res.send('Server On!');
+}
+);
+
+router.use('/user', userRoute)
+router.use('/request', requestRoute)
