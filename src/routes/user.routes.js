22ea0acc9f6753262b86eba69650d5df472120ad
ArthBG <arthurborges.senai564@gmail.com>
2024-09-09 09:01:03 -0300

    feat: Create user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,6 @@ const usersController = require('../controllers/userController');
 router.post('/user', usersController.createUser);
 router.get('/user', usersController.getUser);
 router.get('/user/:name', usersController.getUserByName);
-router.delete('/user/:id', usersController.deleteUser);
+router.delete('/user/:email', usersController.deleteUser);
 
 module.exports = router;
