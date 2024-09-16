3e9e5e6546e413d6e32428785cb5a853e29a1f92
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-09-16 14:41:40 -0300

    feat: .env in database.config

--- src/database/database.config.js
+++ src/database/database.config.js
@@ -3,11 +3,11 @@ const { Pool } = require('pg');
 
 //config without dotenv and local db for tests
 const pool = new Pool({
-    host: 'localhost',
-    port: 7777,
-    user: 'postgres',
-    password: 'ds564',
-    database: 'bflow'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 module.exports = pool;
