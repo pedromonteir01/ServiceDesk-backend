9de1443888fc8230c8387606c214af8edfe689f3
ArthBG <arthurborges.senai564@gmail.com>
2024-09-16 08:24:13 -0300

    refactor: Update routes configuration in index.routes.js and request.routes.js

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const requestRoute = require('./request.routes')
-const userRoute = require('./user.routes')
+const requestRoute = require('./request.routes');
+const userRoute = require('./user.routes');
 
-router.get('/', (req, res) => {
-    res.send('Server On!');
-}
-);
+router.use('/user', userRoute);
+router.use('/request', requestRoute);
 
-router.use('/user', userRoute)
-router.use('/request', requestRoute)
+module.exports = router;
