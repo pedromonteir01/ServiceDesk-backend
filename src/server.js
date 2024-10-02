d069154a7ee3afdea5036a1b408c7f1311e74ee4
Gabriel Caurin <gabrielcaurin@gmail.com>
2024-10-02 10:51:31 -0300

    feat: arrumando o back para receber imagens do front

--- src/server.js
+++ src/server.js
@@ -1,17 +1,41 @@
 require("dotenv").config(); // Carrega as variáveis de ambiente
 const express = require("express"); // O express serve para criar o servidor
 const cors = require("cors"); // O cors serve para permitir requisições de outros domínios
-const router = require('./routes/index.routes');
+const indexRoutes = require("./routes/index.routes"); // Importa as rotas
+const { Query } = require("pg");
+const { populate } = require("dotenv");
+const uploadUser = require("./../middlewares/uploadimage"); // Importa o middleware de upload de imagem
 
 const app = express(); // Cria o servidor e armazena na variável app
 const port = process.env.PORT || 5000; // Pega a porta do arquivo .env ou usa a porta 5000
 
-app.use(express.json()); // Configura o servidor para receber requisições com o formato JSON
 app.use(cors()); // Configura o servidor para aceitar requisições de outros domínios
-app.use('/', router); // Configura o servidor para usar as rotas
+app.use("/", indexRoutes); // Configura o servidor para usar as rotas
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-PINGOTHER ,Content-Type , Authorization"
+  );
+  app.use(express.json()); // Configura o servidor para receber requisições com o formato JSON
+});
+
+app.post("/upload-image", uploadUser.single("image"), async (req, res) => {
+  if (req.file) {
+    return res.json({
+      error: false,
+      message: "Image uploaded successfully!",
+    });
+  }
+  return res.status(400).json({
+    error: true,
+    message: "Error uploading image",
+  });
+});
 
 // Inicia o servidor na porta configurada
 app.listen(port, () =>
   console.log(`⚡ Server started on http://localhost:${port}`)
 );
-
