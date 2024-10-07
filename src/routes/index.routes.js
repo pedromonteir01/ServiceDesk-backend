408f7cc0ffaa3ea0505f303f709d72ff673c1b35
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-10-07 09:21:09 -0300

    feat: getData route & controller

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const usersRouter = require('./users.routes');
 const requestsRouter = require('./requests.routes');
+const dataRouter = require('./getData.routes');
 
 //rotas da aplicação
 const router = Router();
 router.use('/users', usersRouter);
 router.use('/requests', requestsRouter);
+router.use('/data', dataRouter);
 
 module.exports = router;
