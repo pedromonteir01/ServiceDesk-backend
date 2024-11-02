a667896a03184fea835ee06b74ef3aea6f2aa610
Caique <caiquenaimi.564@gmail.com>
2024-11-02 14:33:04 -0300

    refactor: remove image upload middleware and related routes

--- src/routes/requests.routes.js
+++ src/routes/requests.routes.js
@@ -1,7 +1,5 @@
 const { Router } = require('express');
 const requestsController = require('../controllers/requests.controller');
-const upload = require('../middlewares/multer');
-//inicializa a rota
 const requestsRouter = Router();
 
 //métodos
@@ -10,7 +8,7 @@ requestsRouter.get('/:id', requestsController.getRequestById);
 requestsRouter.get('/local/:local', requestsController.getRequestByLocal);
 requestsRouter.get('/status/:status', requestsController.getRequestByStatus);
 requestsRouter.get('/user/:user', requestsController.getRequestByUser);
-requestsRouter.post('/', upload.single('image'), requestsController.createRequest);
+requestsRouter.post('/', requestsController.createRequest);
 requestsRouter.put('/:id', requestsController.updateRequest);
 requestsRouter.delete('/:id', requestsController.deleteRequest);
 requestsRouter.get('/title/:title', requestsController.filterRequestsByTitle);
