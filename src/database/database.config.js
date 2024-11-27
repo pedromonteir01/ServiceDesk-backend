4511ae3c6267a35bc970c932a55bea1878749f32
elPedron <pedrohenriquesilva@aluno.senai.br>
2024-11-27 20:36:53 -0300

    fix: config database

--- src/database/database.config.js
+++ src/database/database.config.js
@@ -3,11 +3,15 @@ const { Pool } = require('pg');
 
 //config without dotenv and local db for tests
 const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL
+    /*
+    PARA USO DE BANCO LOCAL:
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME 
+    */
 });
 
 module.exports = pool;
